a09cd5d00d158037937f910bac49471da9c36f5f
denisa.cernatoiu <denisacernatoiu@yahoo.com>
2025-10-16 23:00:09 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("cv.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
